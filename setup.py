efe970350811a8ed3a921ca297c6a813e4214a05
adminlove520 <791751568@qq.com>
2025-02-19 14:53:24 +0800

    fix

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="cve-monitor",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        "requests==2.31.0",
+        "python-dotenv==1.0.0",
+        "beautifulsoup4==4.12.2",
+        "lxml==4.9.3"
+    ]
+) 
