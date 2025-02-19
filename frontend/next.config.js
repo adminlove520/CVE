ba8107a7c3e25e704f431ef9df87b2a0227accf2
adminlove520 <791751568@qq.com>
2025-02-19 15:14:22 +0800

    new fix

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  output: 'export',
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
+  images: {
+    unoptimized: true,
+  },
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
 }
 
-module.exports = nextConfig 
+module.exports = nextConfig
